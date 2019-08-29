14b30bcc7d8103e9f4bed8e7288061b4612ff256
Layza Floriano <layzafloriano@gmail.com>
2019-08-29 15:27:18 -0300

    commit

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,23 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+import { Photo } from '../photo/photo';
+
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
-
-  photos: Object[] = [];
-
-  constructor(private photoService: PhotoService) {}
-
-  ngOnInit(): void {
-    this.photoService.listFromUser('flavio')
-      .subscribe(photos =>{
-        console.log(photos[0].description);
-        this.photos = photos
-      });
-  }
-
+export class PhotoListComponent implements OnInit, OnDestroy {
+  
+  photos: Photo[] = [];
+  filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+  
+  constructor(
+    private activatedRoute: ActivatedRoute
+    ) { }
+    
+    ngOnInit(): void {
+      this.photos = this.activatedRoute.snapshot.data['photos'];
+      this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+      
+    }
+    ngOnDestroy(): void {
+      this.debounce.unsubscribe();
+    }
+    
 }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,19 +1,25 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Photo } from "./photo";
 
 const API = 'http://localhost:3000'
 
 // Quer dizer que é injetável e recebeu o escopo raiz
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 
 export class PhotoService {
-
   constructor(private http: HttpClient) {
   }
 
   listFromUser(userName: string) {
     return this.http
-    .get<Photo[]>(`${API}/flavio/photos`)
+      .get<Photo[]>(`${API}/${userName}/photos`)
+  }
+  listFromUserPaginated(userName: string, page: number) {
+    const params = new HttpParams()
+      .append('page', page.toString());
+
+    return this.http
+      .get<Photo[]>(`${API}/${userName}/photos`, { params })
   }
 }
